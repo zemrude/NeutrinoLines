8d8b6b4f31a7563a883265f4945d483cd4b66def
sebaur <sebastian.baur@gmail.com>
2020-06-24 10:20:50 -0500

    fix sensitivity plotting

--- plotting/plot_Sensitivities.py
+++ plotting/plot_Sensitivities.py
@@ -18,12 +18,12 @@ latex = style.latex
 
 plt.rcParams.update({'figure.dpi': 300.})
 
-myStyle.increaseAxisText(10)
-myStyle.increaseLegendText(1)
+style.increaseAxisText(10)
+style.increaseLegendText(1)
 
 
-def extractMedianLimit(LECut,HECut,mode, channel, syst, profile, m_min, m_max, binning1, binning2, oversampling=100, CL='_90CL',test=''):
-    all_masses = [10, 16, 25, 40, 63, 100, 158, 251, 398, 631, 1000, 1585, 2512, 3981, 6310, 10000, 15850, 25120, 39810]
+def extractMedianLimit(LECut,HECut,mode, channel, syst, profile, m_min, m_max, binning1, binning2, oversampling=100, CL='_90CL'):
+    all_masses = [40, 63, 100, 158, 251, 398, 631, 1000, 1585, 2512, 3981, 6310, 10000, 15850, 25120, 39810]
 
     x = []
     y = []
@@ -31,13 +31,23 @@ def extractMedianLimit(LECut,HECut,mode, channel, syst, profile, m_min, m_max, b
         if m < m_min or m>m_max:
             continue
             
-        resultFile = base_path+'sensitivity/'+mode+'/'+channel+'/'+syst+'/'+test+'Sensitivity_effective_'+syst+'_'+mode+'_'+profile+'profile_LEBDT'+str(LECut)+'_HEBDT'+str(HECut)+'_'+channel+'_'+str(int(m))+'GeV_binsmerged_'+str(binning1)+'-'+str(binning2)+'_oversampling'+str(oversampling)+CL+'.npy'
+        
+        sensFile_name = 'Sensitivity_effective_'+syst+'_'+mode+'_'+profile+'profile_LEBDT'+str(LECut)+'_HEBDT'+str(HECut)+'_'+channel+'_'+str(int(m))+'GeV_binsmerged_'+str(binning1)+'-'+str(binning2)+'_oversampling'+str(oversampling)+CL+'.npy'
+        sensFile = base_path+'sensitivity/'+mode+'/'+channel+'/'+syst+'/'+sensFile_name
         try: 
-            results = np.load(resultFile).item()
+            results = np.load(sensFile).item()
             x.append(results['mass'])
             y.append(results['median'])
         except:
-            print 'problem with file',resultFile[len(basepath):]
+            if (LECut>0.) and (m>1000):
+                continue # LE Cut and HE mass
+            if (HECut>0.2) and (m<1000):
+                continue # HE Cut and LE mass
+            if channel=='W' and m<180:
+                continue # smaller W mass
+            else:
+                print 'file',sensFile_name,'not found'
+                
     return x,y    
 
 
@@ -52,17 +62,26 @@ def extractBrazilLimit(LECut,HECut,mode, channel,syst, profile, m_min, m_max, bi
     for m in all_masses:
         if m < m_min or m>m_max:
             continue
-                        
-        resultFile = base_path+'sensitivity/'+mode+'/'+channel+'/'+syst+'/Sensitivity_effective_'+syst+'_'+mode+'_'+profile+'profile_LEBDT'+str(LECut)+'_HEBDT'+str(HECut)+'_'+channel+'_'+str(int(m))+'GeV_binsmerged_'+str(binning1)+'-'+str(binning2)+'_oversampling'+str(oversampling)+CL+'.npy'
+          
+        sensFile_name = 'Sensitivity_effective_'+syst+'_'+mode+'_'+profile+'profile_LEBDT'+str(LECut)+'_HEBDT'+str(HECut)+'_'+channel+'_'+str(int(m))+'GeV_binsmerged_'+str(binning1)+'-'+str(binning2)+'_oversampling'+str(oversampling)+CL+'.npy'
+        sensFile = base_path+'sensitivity/'+mode+'/'+channel+'/'+syst+'/'+sensFile_name
         try: 
-            results = np.load(resultFile).item()
+            results = np.load(sensFile).item()
             x.append(results['mass'])
             
             for key in ['median','error_95_low','error_95_high','error_68_low','error_68_high']:
                 y[key].append(results[key])
 
         except:
-            print 'problem with file',resultFile[len(basepath):]
+            if (LECut>0.) and (m>1000):
+                continue # LE Cut and HE mass
+            if (HECut>0.2) and (m<1000):
+                continue # HE Cut and LE mass
+            if channel=='W' and m<180:
+                continue # smaller W mass
+            else:
+                print 'file',sensFile_name,'not found'
+
     return x,y    
 
 def sens_fig_ann():
@@ -110,6 +129,7 @@ def plot_results(mode, channel, profile):
     color['Fermi']='cyan'
     color['Fermi_dec']='cyan'
     color['HAWK']='orange'
+    color['SuperK']='magenta'
     
     ls={}
     ls['ANTARES']='-.'
@@ -121,6 +141,7 @@ def plot_results(mode, channel, profile):
     ls['Fermi']='-.'
     ls['Fermi_dec']='-.'
     ls['HAWK']='-.'
+    ls['SuperK']=':'
 
     label={}
     label['ANTARES']='ANTARES 90% CL [PLB 769 (2017), PLB 796 (2019)]'
@@ -132,6 +153,7 @@ def plot_results(mode, channel, profile):
     label['Fermi'] = 'Fermi-LAT + MAGIC 95% CL (dSph) [JCAP 1602 (2016)]'
     label['Fermi_dec'] = 'Fermi-LAT 95% CL [APJ 761 (2012)]'
     label['HAWK'] = 'HAWK 95% CL (dSph) [APJ 853 (2018)]'
+    label['SuperK'] = 'SuperK 90% CL [arXiv:2005.05106]'
 
     if mode == 'annihilation':
         
@@ -147,7 +169,7 @@ def plot_results(mode, channel, profile):
             except:
                 continue
                 
-        for ex in ['Fermi','ANTARES','ICDC','IC86']:
+        for ex in ['SuperK','Fermi','ANTARES','ICDC','IC86']:
             filename = base_path+'resources/paperLimits/annihilation/'+ex+'/'+ex+'_'+channel+'_'+profile+'.csv'
             try:
                 inFile = open(filename, 'r')
@@ -210,10 +232,13 @@ def plot_results(mode, channel, profile):
                 ax.plot(x,y, lw=3,ls=ls[ex],color=color[ex],label=label[ex])
             except:
                 continue
-                
+      
+    
+    
+    # This work
+    
     oversampling_LE = -1
     oversampling_HE = -1
-    
     oversampling_HE_Burkert = -1
 
         
@@ -228,10 +253,7 @@ def plot_results(mode, channel, profile):
         if profile == 'Burkert':
             oversampling_HE = 100
         
-    # This work
     transition = 900
-    if mode == 'decay':
-        transition = 900
     sens_combined_1 = extractBrazilLimit(0.15,0.2,mode,channel,'nominal',profile,30,transition, 2, 5, oversampling_LE, '_90CL')
     sens_combined_2 = extractBrazilLimit(-1.0,0.3,mode,channel,'nominal',profile,transition,50000, 2, 5, oversampling_HE, '_90CL')    
 
@@ -365,82 +387,89 @@ fig.savefig('plots/Sensitivity_Samples_decay_nue_NFW.png',bbox_inches = 'tight')
 
 ###############################################
 
+for profile in ['NFW', 'Burkert']:
+    
+    oversampling_nue_LE = 100
+    oversampling_nue_HE = 100
+    if profile == 'NFW':
+        oversampling_nue_HE = 200
+    
 
-fig,ax = sens_fig_ann()
+    fig,ax = sens_fig_ann()
 
-channels = ['tau','mu','b','W']
+    channels = ['tau','mu','b','W']
 
-sens_1 = {}
-sens_2 = {}
-sens_combined = {}
+    sens_1 = {}
+    sens_2 = {}
+    sens_combined = {}
 
-c='nue'
-sens_1[c] = extractMedianLimit(0.15,0.2,'annihilation',c,'nominal','NFW',30,900, 2, 5, 100, '_90CL')
-sens_2[c] = extractMedianLimit(-1.0,0.3,'annihilation',c,'nominal','NFW',900,50000, 2, 5, 200, '_90CL')    
-sens_combined[c] = []
-sens_combined[c].append(np.append(np.array(sens_1[c][0]),np.array(sens_2[c][0])))
-sens_combined[c].append(np.append(np.array(sens_1[c][1]),np.array(sens_2[c][1])))
-
-ax.plot(sens_combined[c][0],sens_combined[c][1],lw=3,label=latex[c])
-
-
-for c in channels:
-    sens_1[c] = extractMedianLimit(0.15,0.2,'annihilation',c,'nominal','NFW',30,900, 2, 5, -1, '_90CL')
-    sens_2[c] = extractMedianLimit(-1.0,0.3,'annihilation',c,'nominal','NFW',900,50000, 2, 5, -1, '_90CL')    
+    c='nue'
+    sens_1[c] = extractMedianLimit(0.15,0.2,'annihilation',c,'nominal',profile,30,900, 2, 5, oversampling_nue_LE, '_90CL')
+    sens_2[c] = extractMedianLimit(-1.0,0.3,'annihilation',c,'nominal',profile,900,50000, 2, 5, oversampling_nue_HE, '_90CL')    
     sens_combined[c] = []
     sens_combined[c].append(np.append(np.array(sens_1[c][0]),np.array(sens_2[c][0])))
     sens_combined[c].append(np.append(np.array(sens_1[c][1]),np.array(sens_2[c][1])))
-    
+
     ax.plot(sens_combined[c][0],sens_combined[c][1],lw=3,label=latex[c])
 
 
-ax.set_ylim(1e-25,1e-21)
-plt.text(12*1e3, 1e-21/2, 'NFW profile',fontsize=17, color = 'black')
+    for c in channels:
+        sens_1[c] = extractMedianLimit(0.15,0.2,'annihilation',c,'nominal',profile,30,900, 2, 5, -1, '_90CL')
+        sens_2[c] = extractMedianLimit(-1.0,0.3,'annihilation',c,'nominal',profile,900,50000, 2, 5, -1, '_90CL')    
+        sens_combined[c] = []
+        sens_combined[c].append(np.append(np.array(sens_1[c][0]),np.array(sens_2[c][0])))
+        sens_combined[c].append(np.append(np.array(sens_1[c][1]),np.array(sens_2[c][1])))
 
-leg = ax.legend(frameon = 1, fancybox=False, loc='lower left', bbox_to_anchor=(-0.0175, 1., 1.035, 0),labelspacing=0.2, mode="expand", ncol=1)
-fig.tight_layout()
-fig.savefig('plots/Sensitivity_annihilation_channels_NFW.pdf',bbox_inches = 'tight') 
-fig.savefig('plots/Sensitivity_annihilation_channels_NFW.png',bbox_inches = 'tight') 
-
-
-###############################################
-#--------------------- decay
-
-fig,ax = sens_fig_dec()
-
-channels = ['tau','mu','b','W']
-
-sens_1 = {}
-sens_2 = {}
-sens_combined = {}
-
-c='nue'
-sens_1[c] = extractMedianLimit(0.15,0.2,'decay',c,'nominal','NFW',30,900, 2, 5, 100, '_90CL')
-sens_2[c] = extractMedianLimit(-1.0,0.3,'decay',c,'nominal','NFW',900,50000, 2, 5, 100, '_90CL')    
-sens_combined[c] = []
-sens_combined[c].append(np.append(np.array(sens_1[c][0]),np.array(sens_2[c][0])))
-sens_combined[c].append(np.append(np.array(sens_1[c][1]),np.array(sens_2[c][1])))
-
-ax.plot(sens_combined[c][0],sens_combined[c][1],lw=3,label=latex[c])
+        ax.plot(sens_combined[c][0],sens_combined[c][1],lw=3,label=latex[c])
 
 
-for c in channels:
-    sens_1[c] = extractMedianLimit(0.15,0.2,'decay',c,'nominal','NFW',30,900, 2, 5, -1, '_90CL')
-    sens_2[c] = extractMedianLimit(-1.0,0.3,'decay',c,'nominal','NFW',900,50000, 2, 5, -1, '_90CL')    
+    ax.set_ylim(1e-25,1e-21)
+    plt.text(12*1e3, 1e-21/2, profile+' profile',fontsize=17, color = 'black')
+
+    leg = ax.legend(frameon = 1, fancybox=False, loc='lower left', bbox_to_anchor=(-0.0175, 1., 1.035, 0),labelspacing=0.2, mode="expand", ncol=1)
+    fig.tight_layout()
+    fig.savefig('plots/Sensitivity_annihilation_channels_'+profile+'.pdf',bbox_inches = 'tight') 
+    fig.savefig('plots/Sensitivity_annihilation_channels_'+profile+'.png',bbox_inches = 'tight') 
+
+
+    ###############################################
+    #--------------------- decay
+
+    fig,ax = sens_fig_dec()
+
+    channels = ['tau','mu','b','W']
+
+    sens_1 = {}
+    sens_2 = {}
+    sens_combined = {}
+
+    c='nue'
+    sens_1[c] = extractMedianLimit(0.15,0.2,'decay',c,'nominal',profile,30,900, 2, 5, oversampling_nue_LE, '_90CL')
+    sens_2[c] = extractMedianLimit(-1.0,0.3,'decay',c,'nominal',profile,900,50000, 2, 5, 100, '_90CL')    
     sens_combined[c] = []
     sens_combined[c].append(np.append(np.array(sens_1[c][0]),np.array(sens_2[c][0])))
     sens_combined[c].append(np.append(np.array(sens_1[c][1]),np.array(sens_2[c][1])))
-    
+
     ax.plot(sens_combined[c][0],sens_combined[c][1],lw=3,label=latex[c])
 
 
-ax.set_ylim(1e22,1e29)
-plt.text(12*1e3, 1e29/2, 'NFW profile',fontsize=17, color = 'black')
+    for c in channels:
+        sens_1[c] = extractMedianLimit(0.15,0.2,'decay',c,'nominal',profile,30,900, 2, 5, -1, '_90CL')
+        sens_2[c] = extractMedianLimit(-1.0,0.3,'decay',c,'nominal',profile,900,50000, 2, 5, -1, '_90CL')    
+        sens_combined[c] = []
+        sens_combined[c].append(np.append(np.array(sens_1[c][0]),np.array(sens_2[c][0])))
+        sens_combined[c].append(np.append(np.array(sens_1[c][1]),np.array(sens_2[c][1])))
 
-leg = ax.legend(frameon = 1, fancybox=False, loc='lower left', bbox_to_anchor=(-0.0175, 1., 1.035, 0),labelspacing=0.2, mode="expand", ncol=1)
-fig.tight_layout()
-fig.savefig('plots/Sensitivity_decay_channels_NFW.pdf',bbox_inches = 'tight') 
-fig.savefig('plots/Sensitivity_decay_channels_NFW.png',bbox_inches = 'tight') 
+        ax.plot(sens_combined[c][0],sens_combined[c][1],lw=3,label=latex[c])
+
+
+    ax.set_ylim(1e22,1e29)
+    plt.text(12*1e3, 1e29/2, profile+' profile',fontsize=17, color = 'black')
+
+    leg = ax.legend(frameon = 1, fancybox=False, loc='lower left', bbox_to_anchor=(-0.0175, 1., 1.035, 0),labelspacing=0.2, mode="expand", ncol=1)
+    fig.tight_layout()
+    fig.savefig('plots/Sensitivity_decay_channels_'+profile+'.pdf',bbox_inches = 'tight') 
+    fig.savefig('plots/Sensitivity_decay_channels_'+profile+'.png',bbox_inches = 'tight') 
 
 
 ###############################################
