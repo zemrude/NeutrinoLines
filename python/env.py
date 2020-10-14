5c12472b653c7be15fa510cb1ee37d17274063d3
Juanan Aguilar <aguilar@cobalt06.icecube.wisc.edu>
2020-10-14 03:57:30 -0500

    removing env.py

--- python/env.py
+++
@@ -1,5 +0,0 @@
-import sys, os
-
-os.environ["ANALYSIS_BASE_PATH"]="/data/ana/BSM/HT_Cascade/FinalAnalysisCode/"
-os.environ["ANALYSIS_DATA_DIR"]="/data/ana/BSM/HT_Cascade/FinalAnalysisCode/data/"
-
